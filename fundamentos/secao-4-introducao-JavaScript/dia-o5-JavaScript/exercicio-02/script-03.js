efc7483858466369c936d255b29b2e83e240b7e6
Nathan Crevelin <neitass.music@gmail.com>
2022-12-01 19:24:40 -0300

    termina exercicio-02.03

--- fundamentos/secao-4-introducao-JavaScript/dia-o5-JavaScript/exercicio-02/script-03.js
+++ fundamentos/secao-4-introducao-JavaScript/dia-o5-JavaScript/exercicio-02/script-03.js
@@ -1,1 +1,13 @@
+const longestWord = (phase) => {
+let jeffDammer = phase.split(' ');
+let saveWord = '';
+console.log(saveWord)
+for (let index = 0; index < jeffDammer.length; index+=1) {
+    if (jeffDammer[index].length > saveWord.length) {
+        saveWord = jeffDammer[index];
+    }
+    
+}
+return console.log(saveWord)
+} 
 longestWord('Antônio foi no banheiro e não sabemos o que aconteceu') // retorna 'aconteceu'
