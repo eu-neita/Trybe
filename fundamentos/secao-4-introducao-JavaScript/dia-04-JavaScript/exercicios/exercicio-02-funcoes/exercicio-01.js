3bb25e89d1a4f5015e0c84f029b9ed73fcdae6ae
Nathan Crevelin <neitass.music@gmail.com>
2022-12-01 11:26:10 -0300

    adiciona exercicio-02 dia 4-03/ainda incompleto

--- fundamentos/secao-4-introducao-JavaScript/dia-04-JavaScript/exercicios/exercicio-02-funcoes/exercicio-01.js
+++ fundamentos/secao-4-introducao-JavaScript/dia-04-JavaScript/exercicios/exercicio-02-funcoes/exercicio-01.js
@@ -1,13 +1,13 @@
 function verificaPalindrome(phases,){
     let phaseRev = phases.split('').reverse().join('')
 if (phases === phaseRev) {
-    return true;
+    return console.log('palindromo é ' + true);
 }else{
-    return false
+    return console.log('palindromo é ' + false);
 }
 }
-console.log(verificaPalindrome('arara'));
-console.log(verificaPalindrome('desenvolvimento'));
+verificaPalindrome('arara');
+verificaPalindrome('desenvolvimento');
 let maior = 0;
 
 function maiorNumero(array) {
@@ -19,7 +19,7 @@ function maiorNumero(array) {
     return maior;
 }
 
-console.log(maiorNumero([2, 3, 6, 7, 10, 1]));
+console.log('maior numero é: ' +maiorNumero([2, 3, 6, 7, 10, 1]));
 
 let menor = 0;
 
@@ -31,5 +31,50 @@ function menorNumero(array) {
     }
     return menor;
 }
+let maiorNome = '';
+console.log('menor numero é: ' +menorNumero([2, 4, 6, 7, 10, 0, -3]));
 
-console.log(menorNumero([2, 4, 6, 7, 10, 0, -3]));
+function namesArray(array) {
+    for (let index = 0; index < array.length; index+=1) {
+        if(maiorNome.length < array[index].length)
+        maiorNome = array[index];
+
+    }
+    return console.log('o maior nome é: '+maiorNome);
+}
+
+namesArray(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']);
+let armazaenaNumero = 0;
+function inteiro(array) {
+    for (let index = 0; index < array.length; index+=1) {
+        for (let index2 = 0; index2 < array.length+1; index2+=1) {
+            if (array[index] === array[index2]) {
+            armazaenaNumero = array[index];
+            }   
+            
+        }
+        return console.log('numero inteiro que mais se repete é: '+armazaenaNumero);
+        
+    }
+}
+inteiro([2, 3, 2, 5, 8, 2, 3]);
+let somaNumeros = 0;
+function somatorio(number) {
+    for (let index = 0 ; index < number+1; index+=1) {
+        somaNumeros += index;
+    }
+    return console.log('resultado das somatórias: '+ somaNumeros);
+}
+
+somatorio(5);
+
+function verifica(word, ending) {
+    for (let index = 0; index < word.length; index+=1) {
+        if(word[index] == ending[1] && word[index+1] == ending[2]){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+verifica('manoNaoFunciona','na')
