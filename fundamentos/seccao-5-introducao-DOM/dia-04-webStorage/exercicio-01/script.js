9114e6fa38f9c755232b84b699a9867e7fc96bb5
neita <neitass.music@gmail.com>
2022-12-13 20:43:43 -0300

    falta adicionar funcao salva tipo-fonte

--- fundamentos/seccao-5-introducao-DOM/dia-04-webStorage/exercicio-01/script.js
+++ fundamentos/seccao-5-introducao-DOM/dia-04-webStorage/exercicio-01/script.js
@@ -3,7 +3,9 @@ const bgColor = ['white','black','green','blue','yellow']
 const buttonsFontC = document.querySelectorAll('#font-color button');
 const fontColor = ['black','red','white'];
 const buttonsFontS = document.querySelectorAll('#font-size button')
+const sizeFont = ['8','10','12','14','20'];
 const buttonsLineH = document.querySelectorAll('#line-height button')
+const fontHeight = ['1','1.15','1.5','2.0','3.0'];
 const buttonsFontStyle = document.querySelectorAll('#font-family button')
 
 function chageBackColor() {
@@ -50,36 +52,46 @@ chageTextColor()
 function chageFontSize() {
     buttonsFontS[0].addEventListener('click', function () {
         document.body.style.fontSize = '8pt'
+        localStorage.setItem('fontSize', sizeFont[0]);
     })
     buttonsFontS[1].addEventListener('click', function () {
         document.body.style.fontSize = '10pt'
+        localStorage.setItem('fontSize', sizeFont[1]);
     })
     buttonsFontS[2].addEventListener('click', function () {
         document.body.style.fontSize = '12pt'
+        localStorage.setItem('fontSize', sizeFont[2]);
     })
     buttonsFontS[3].addEventListener('click', function () {
         document.body.style.fontSize = '14pt'
+        localStorage.setItem('fontSize', sizeFont[3]);
     })
     buttonsFontS[4].addEventListener('click', function () {
         document.body.style.fontSize = '20pt'
+        localStorage.setItem('fontSize', sizeFont[4]);
     })
 }
 chageFontSize()
 function chageLineHeight() {
     buttonsLineH[0].addEventListener('click', function () {
         document.body.style.lineHeight = '1'
+        localStorage.setItem('lineHeight', fontHeight[0]);
     })
     buttonsLineH[1].addEventListener('click', function () {
         document.body.style.lineHeight = '1.15'
+        localStorage.setItem('lineHeight', fontHeight[1]);
     })
     buttonsLineH[2].addEventListener('click', function () {
         document.body.style.lineHeight = '1.5'
+        localStorage.setItem('lineHeight', fontHeight[2]);
     })
     buttonsLineH[3].addEventListener('click', function () {
         document.body.style.lineHeight = '2.0'
+        localStorage.setItem('lineHeight', fontHeight[3]);
     })
     buttonsLineH[4].addEventListener('click', function () {
         document.body.style.lineHeight = '3.0'
+        localStorage.setItem('lineHeight', fontHeight[4]);
     })
 }
 chageLineHeight()
@@ -96,6 +108,8 @@ chageTypeFont()
 function storage() {
     const bglColor = localStorage.getItem('backgroundColor')
     const textColor = localStorage.getItem('colorFont')
+    const sizeFont = localStorage.getItem('fontSize')
+    const heightLine = localStorage.getItem('lineHeight')
 if (bglColor == 'white') {
     document.body.style.backgroundColor = 'white'
 }else if(bglColor === 'black'){
@@ -114,6 +128,30 @@ if (textColor == 'black') {
     document.body.style.color = 'red'
 }else if(textColor === 'white'){
     document.body.style.color = 'white'
+}
+
+if (sizeFont == '8') {
+    document.body.style.fontSize = '8pt'
+}else if(sizeFont === '10'){
+    document.body.style.fontSize = '10pt'
+}else if(sizeFont === '12'){
+    document.body.style.fontSize = '12pt'
+}else if(sizeFont === '14'){
+    document.body.style.fontSize = '14pt'
+}else if(sizeFont === '20'){
+    document.body.style.fontSize = '20pt'
+}
+
+if (heightLine == '1') {
+    document.body.style.lineHeight = '1'
+}else if(heightLine === '1.15'){
+    document.body.style.lineHeight = '1.15'
+}else if(heightLine === '1.5'){
+    document.body.style.lineHeight = '1.5'
+}else if(heightLine === '2.0'){
+    document.body.style.lineHeight = '2.0'
+}else if(heightLine === '3.0'){
+    document.body.style.lineHeight = '3.0'
 }
 
 }
