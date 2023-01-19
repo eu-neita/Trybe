28a21c0df1f233ebd89a235416f663a191e89a34
neita <neitass.music@gmail.com>
2023-01-19 09:19:18 -0300

    salva em objassing

--- fundamentos/seccao-6-jsese6-testes-uni/dia-1/exercicios/03/script.js
+++ fundamentos/seccao-6-jsese6-testes-uni/dia-1/exercicios/03/script.js
@@ -17,3 +17,15 @@ const lesson3 = {
   professor: 'Maria Clara',
   turno: 'noite',
 };
+
+const addTurno = (lesson2, key, value) => lesson2[key] = value;
+addTurno(lesson2, 'turno', 'noite');
+const listKey = (obj) => Object.keys(obj);
+listKey(lesson2)
+const keyLength = (obj) => Object.keys(obj).length;
+console.log(keyLength(lesson2));
+const objValues = (obj) => Object.values(obj);
+console.log(objValues(lesson2));
+
+const objAssing = (obj, obj2, obj3) => Object.assign(obj, obj2, obj3);
+console.log(objAssing(lesson1, lesson2, lesson3));
