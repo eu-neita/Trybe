f2c9266865e48455cac5bd587ee3a6142f850c6e
neita <neitass.music@gmail.com>
2023-01-19 18:42:22 -0300

    termina exercicio 1

--- fundamentos/seccao-6-jsese6-testes-uni/dia-02/exercicios/01/script.test.js
+++ fundamentos/seccao-6-jsese6-testes-uni/dia-02/exercicios/01/script.test.js
@@ -1,7 +1,13 @@
-const myRemove = require('./script');
+const myRemove = require('./script.js');
 
-test('sverifica array', () => {
-
-  expect(myRemove([1, 2, 3, 4])).toBe(3);
-
+describe('testa a função myRemove', () => {
+  it('verifica se array retorna corretamente', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  });
 });
+
+
+
+
