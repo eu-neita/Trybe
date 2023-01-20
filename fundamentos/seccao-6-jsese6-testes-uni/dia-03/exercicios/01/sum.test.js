0b200faeb3d064655a19ea35bd0957ae5ae4c707
neita <neitass.music@gmail.com>
2023-01-20 18:45:32 -0300

    adiciona exerciocio cumpre 2 requisito

--- fundamentos/seccao-6-jsese6-testes-uni/dia-03/exercicios/01/sum.test.js
+++ fundamentos/seccao-6-jsese6-testes-uni/dia-03/exercicios/01/sum.test.js
@@ -1,7 +1,10 @@
 const sum = require('./sum.js');
 
  describe('testar soma!', () => {
-  it('Testa se o retorno de sum(4, 5) é 9; ', () => {
+  it('Testa se o retorno de sum(4, 5) é 9.', () => {
     expect(sum(4, 5)).toEqual(9);
+  });
+  it('Testa se o retorno de sum(0, 0) é 0.', () => {
+    expect(sum(0, 0)).toEqual(0);
   });
  });
