47324b9e3edd6f03d496542cd637adf48bc94f1b
Nathan de assis Crevelin <neitass.music@gmail.com>
2023-03-23 19:42:33 -0300

    adiciona testes

--- front-end/testes-automatizados-com-RTL/dia-01/para-fixar/testes-react/src/App.test.js
+++ front-end/testes-automatizados-com-RTL/dia-01/para-fixar/testes-react/src/App.test.js
@@ -1,8 +1,41 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
 import App from './App';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import ValidEmail from './components/VerifyEmail';
 
-test('renders learn react link', () => {
+test('Verificando se o botão e o campo email estão funcionando.', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const EMAIL_USER = 'email@email.com';
+
+  const textEmail = screen.getByTestId('id-email-user');
+  expect(textEmail).toBeInTheDocument();
+  expect(textEmail).toHaveTextContent('Valor:');
+
+  const btnSend = screen.getByTestId('id-send');
+  const inputEmail = screen.getByLabelText('Email');
+  userEvent.type(inputEmail, EMAIL_USER);
+  userEvent.click(btnSend);
+
+
+  expect(textEmail).toHaveTextContent(`Valor: ${ EMAIL_USER }`);
+  expect(inputEmail).toHaveValue('');
+});
+
+describe('testando se exitste um componente que indica se o email é válido ou inválido', () => {
+
+  test('Testando um componente, caso o email seja válido.', () => {
+    const EMAIL_USER = 'email@email.com';
+    render(<ValidEmail email={ EMAIL_USER } />);
+    const isValid = screen.getByText('Email Válido');
+    expect(isValid).toBeInTheDocument();
+  });
+
+  test('Testando um componente, caso o email seja inválido.', () => {
+    const EMAIL_USER = 'emailerrado';
+    render(<ValidEmail email={ EMAIL_USER } />);
+    const isValid = screen.getByText('Email Inválido');
+    expect(isValid).toBeInTheDocument();
+  });
 });
