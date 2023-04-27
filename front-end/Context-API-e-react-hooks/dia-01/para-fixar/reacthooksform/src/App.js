7faea8cfeb86ddd3730dd1fec1df354f88cb3992
nathan crevelin <neitass.music@gmail.com>
2023-04-27 11:31:11 -0300

    termina para-fixar e exercicios

--- front-end/Context-API-e-react-hooks/dia-01/para-fixar/reacthooksform/src/App.js
+++ front-end/Context-API-e-react-hooks/dia-01/para-fixar/reacthooksform/src/App.js
@@ -1,41 +1,40 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  let [handleInputs, setHandle] = useState({
-    fullName: '',
-    age: 0,
-    module: ''
-  })
-
+  const [inputs, setInputs] = useState({});
+  
   const onChangeInputs = (event) => {
-    let value = event.target.value;
-    let name = event.target.name;
-    setHandle((prev) => {
-      return {
-        ...prev,
-        [name]: value
-      }
-    })
-    console.log(handleInputs);
+    const { name, value } = event.target;
+    setInputs((prev) => ({
+      ...prev,
+      [name]: value
+    }));
   }
   return (
     <div className="App">
       <form className="App-header">
-       <input type="text" name="fullName" onChange={ onChangeInputs } placeholder="Nome completo"/>
-       <input type="number" name="age" onChange={ onChangeInputs }/>
-       <div onChange={ onChangeInputs }>
-       <label>
-       <input type="radio" name='module' value="fundamentos"/>fundamentos
-       </label>
-       <input type="radio" name='module' value="front-end"/>front-end
-       <label>
-       <input type="radio" name='module' value="back-end"/>back-end
-       </label>
-       <label>
-       <input type="radio" name='module' value="ciencia-da-computação"/>ciencia-da-computação
-       </label>
-       </div>
+        <input type="text" name="fullName" value={inputs.fullName} onChange={ onChangeInputs } placeholder="Nome completo"/>
+        <input type="number" name="age" value={inputs.age} onChange={ onChangeInputs }/>
+        <div onChange={ onChangeInputs }>
+
+          <label>
+            <input type="radio" name='module' value="fundamentos"/>fundamentos
+          </label>
+
+          <label>
+            <input type="radio" name='module' value="front-end"/>front-end
+          </label>
+
+          <label>
+            <input type="radio" name='module' value="back-end"/>back-end
+          </label>
+
+          <label>
+            <input type="radio" name='module' value="ciencia-da-computação"/>ciencia-da-computação
+          </label>
+          
+        </div>
        <button>Submeter</button>
       </form>
     </div>
