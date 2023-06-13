b243c84e8a2f9cfe17ede3f0600cd17bf7e554ba
Neita <neita-macos@Neitas-iMac-Pro.local>
2023-06-13 08:04:46 -0300

    termina condiçoes de obesidade

--- back-end/nodejs-runtime-async/exercicios/exercicio01.js
+++ back-end/nodejs-runtime-async/exercicios/exercicio01.js
@@ -1,2 +1,14 @@
-const imc = (we, he) => we/(he*he)
-console.log(imc(80, 1.72));
+const readLine = require('readline-sync')
+
+const imc = () => {
+  const weight = readLine.question('What’s your weight?');
+  const heigth = readLine.questionFloat('What’s your height?');
+  const calc = weight/(heigth*heigth);
+  if (calc < 18.5) return console.log('abaixo do peso')
+  if (calc > 18.5 && calc <= 24.9) return console.log(calc + ' peso normal')
+  if (calc >= 25 && calc <= 29.9) return console.log(calc + ' sobre-peso')
+  if (calc >= 30 && calc <= 34.9) return console.log(calc + ' obesidade grau 1')
+  if (calc >= 35 && calc <= 39.9) return console.log(calc + ' obesidade grau 2')
+  return console.log("obesidade graus 3 e 4");
+}
+imc();
